02d970445046ddbe6fcf11130225988e8d768b8d
alfata <123697185+alf4ta@users.noreply.github.com>
2023-05-23 22:02:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,34 @@ import os
 from pystyle import Colorate, Colors, Center
 from selenium.webdriver.chrome.options import Options
 import time
+import colorama
+import msvcrt
+
+github = "ur github url"
+green = "\033[38;5;83m"
+red = "\033[38;5;203m"
+white = "\033[38;5;231m"
 
 driver_path = 'chromedriver.exe'
 
 chrome_options = Options()
-chrome_options.add_argument('--headless') 
+headless_mode = True
 
-driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
+def toggle_headless_mode():
+    global headless_mode
+    headless_mode = not headless_mode
 
-driver.get('ur github link')
+toggle_key = b'p'  # You can change the toggle keybind here
+
+driver = None
+
+if not headless_mode:
+    driver = webdriver.Chrome(executable_path=driver_path)
+else:
+    chrome_options.add_argument('--headless')
+    driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
+
+driver.get(github)
 
 os.system('cls')
 
@@ -30,13 +49,26 @@ $$  __$$ |$$ |$$ |            $$ |  $$ |$$\ $$  __$$ |
  \_______|\__|\__|            \__|   \____/  \_______|                                 
     """, 1))
 os.system(f'title https://github.com/alf4ta')
+
 while counter < num_refreshes:
     driver.refresh()
     counter += 1
     os.system('cls')
     print(banner)
     print("Repo: https://github.com/alf4ta/github-view-bot")
-    print(f"Views: {counter}")
+    print(f"Views: \033[38;5;133m{counter}{white} - Headless: {f'{green}ENABLED{white}' if headless_mode else f'{red}DISABLED{white}'} - headless keybind: \033[38;5;133mP{white}")
     os.system(f'title Alf4ta ViewBot - Views - {counter}')
+    
+    if msvcrt.kbhit():
+        key = msvcrt.getch()
+        if key == toggle_key:
+            toggle_headless_mode()
+            driver.quit()  # Quit the current driver instance
+            if not headless_mode:
+                driver = webdriver.Chrome(executable_path=driver_path)
+            else:
+                chrome_options.add_argument('--headless')
+                driver = webdriver.Chrome(executable_path=driver_path, options=chrome_options)
+            driver.get(github)
 
 driver.quit()
